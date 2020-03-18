7ae9435cbdff4cf95b33fbcf5f6dd1525d6ba3fa
Mark Solomonik <marik.solomonik@gmail.com>
2020-03-17 18:04:59 -0700

    Added nlp package

--- src/TrainScreen.js
+++ src/TrainScreen.js
@@ -2,13 +2,42 @@ import React from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import "react-native-gesture-handler";
 // const { NlpManager } = require('node-nlp');
-import NlpManager from "node-nlp-rn";
+// import NlpManager from "node-nlp-rn";
+const { containerBootstrap } = require('@nlpjs/core');
+const { Nlp } = require('@nlpjs/nlp');
+const { LangEn } = require('@nlpjs/lang-en-min');
 
-// import natural from "natural";
 export default class TrainScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = { text: "" };
+  }
+
+  componentDidMount = async () => {
+    const container = await containerBootstrap();
+    container.use(Nlp);
+    container.use(LangEn);
+    const nlp = container.get('nlp');
+    nlp.settings.autoSave = false;
+    nlp.addLanguage('en');
+    // Adds the utterances and intents for the NLP
+    nlp.addDocument('en', 'goodbye for now', 'greetings.bye');
+    nlp.addDocument('en', 'bye bye take care', 'greetings.bye');
+    nlp.addDocument('en', 'okay see you later', 'greetings.bye');
+    nlp.addDocument('en', 'bye for now', 'greetings.bye');
+    nlp.addDocument('en', 'i must go', 'greetings.bye');
+    nlp.addDocument('en', 'hello', 'greetings.hello');
+    nlp.addDocument('en', 'hi', 'greetings.hello');
+    nlp.addDocument('en', 'howdy', 'greetings.hello');
+    
+    // Train also the NLG
+    nlp.addAnswer('en', 'greetings.bye', 'Till next time');
+    nlp.addAnswer('en', 'greetings.bye', 'see you soon!');
+    nlp.addAnswer('en', 'greetings.hello', 'Hey there!');
+    nlp.addAnswer('en', 'greetings.hello', 'Greetings!');
+    await nlp.train();
+    const response = await nlp.process('en', 'Well, take a seat!');
+    console.log(response);
   }
   render() {
 
@@ -25,30 +54,6 @@ export default class TrainScreen extends React.Component {
     );
   }
 }
-const manager = new NlpManager({ languages: ['en'] });
-// Adds the utterances and intents for the NLP
-// manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-// manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-// manager.addDocument('en', 'okay see you later', 'greetings.bye');
-// manager.addDocument('en', 'bye for now', 'greetings.bye');
-// manager.addDocument('en', 'i must go', 'greetings.bye');
-// manager.addDocument('en', 'hello', 'greetings.hello');
-// manager.addDocument('en', 'hi', 'greetings.hello');
-// manager.addDocument('en', 'howdy', 'greetings.hello');
-
-// // Train also the NLG
-// manager.addAnswer('en', 'greetings.bye', 'Till next time');
-// manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-// manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-// manager.addAnswer('en', 'greetings.hello', 'Greetings!');
-
-// // Train and save the model.
-// (async() => {
-//     await manager.train();
-//     manager.save();
-//     const response = await manager.process('en', 'I should go now');
-//     console.log(response);
-// })();
 
 const styles = StyleSheet.create({
   container: {
